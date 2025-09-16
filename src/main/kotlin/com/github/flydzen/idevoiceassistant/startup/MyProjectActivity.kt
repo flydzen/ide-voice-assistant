e3b98e4cdffeb0448eef21457117d7de19d3cf7c
Gleb Oborin <gleb.oborin@jetbrains.com>
2025-09-16 18:02:23 +0200

    Do not disable idea vim plugin during startup

--- src/main/kotlin/com/github/flydzen/idevoiceassistant/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/flydzen/idevoiceassistant/startup/MyProjectActivity.kt
@@ -5,14 +5,11 @@ import com.intellij.ml.llm.codeGeneration.AICodeGenerationListener
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.maddyhome.idea.vim.VimPlugin
 
 class MyProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
-        VimPlugin.setEnabled(false)
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-
         AutoCodeGenerationListener.promptText = "TODO"
         project.messageBus.connect()
             .subscribe(AICodeGenerationListener.TOPIC, AutoCodeGenerationListener(project))
