de3172e93abad337d2f165b7eb78379214d8e6fc
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2025-09-16 12:15:53 +0200

    Add vim-engine dependency to Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
+    implementation(libs.vim.engine)
+
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
 
